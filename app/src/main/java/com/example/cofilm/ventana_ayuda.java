94262dc155008206d63b46774a9fe79a8d13b22f
jeison <jeison.klinger.mt@usa.edu.co>
2022-12-22 00:49:19 -0500

    se agrega diseño visual

---
+++ app/src/main/java/com/example/cofilm/ventana_ayuda.java
@@ -0,0 +1,14 @@
+package com.example.cofilm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ventana_ayuda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_ayuda);
+    }
+}
